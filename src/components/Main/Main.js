8997d1da060417306a15c9b337d165bed83df25d
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-29 00:28:03 +0500

    refactor(Main): transfer info education in separate component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,28 +1,33 @@
 import './Main.css';
 import React from "react";
+import PropTypes from 'prop-types';
 import CurrentDataInfoContext from '../../context/CurrentDataInfoContext';
 import PortfolioContainer from '../PortfolioContainer/PortfolioContainer';
+import EducationContainer from '../EducationContainer/EducationContainer';
+import BtnSwitchTheme from '../BtnSwitchTheme/BtnSwitchTheme';
 import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
-function Main() {
+function Main({ setTheme }) {
   const pageDataInfo = React.useContext(CurrentDataInfoContext);
   const theme = React.useContext(CurrentThemeContext);
   return (
     <main className='main'>
-      <h1 className='main__title'>{pageDataInfo.name}</h1>
-      <h2 className={`main__subtitle main__subtitle_profession`}>{pageDataInfo.profession}</h2>
+      <div className='main__block-title'>
+        <h1 className='main__title'>{pageDataInfo.name}</h1>
+        <BtnSwitchTheme setTheme={setTheme} />
+      </div>
+      <h2 className={`main__subtitle main__subtitle_profession`}>{pageDataInfo.profession.toLowerCase()}</h2>
       <h2 className={`main__subtitle main__subtitle_stack`}>
         Stack:&nbsp;<span className='main__subtitle-text-elem'>{pageDataInfo.stack}</span>
       </h2>
       <PortfolioContainer />
-      <h2 className={`main__subtitle main__subtitle_education`}>Education:&nbsp;
-        <a href={pageDataInfo.educationLink} className={`main__subtitle-text-elem main__subtitle-text-elem_link`} target={'_blank'} rel="noreferrer">
-          <span className={`portfolio-elem-container__image-open-page portfolio-elem-container__image-open-page_theme-${theme}`} />Yandex.Practicum.
-        </a>
-      </h2>
-      {/* контейнер */}
+      <EducationContainer />
     </main>
   );
 }
+
+Main.propTypes = {
+  setTheme: PropTypes.object,
+};
 
 export default Main;
