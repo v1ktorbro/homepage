6f14beb719b0b23700db75ffb95844ee6ec97cef
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-29 00:23:07 +0500

    refactor(App): remove header and transfer setTheme to Main component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
-import Header from '../Header/Header';
 import Main from '../Main/Main';
 import { CurrentThemeContext, defaultTheme} from '../../context/CurrentThemeContext';
+import CurrentDataInfoContext from '../../context/CurrentDataInfoContext';
+import { dataInfo } from '../../utils/dataInfo';
 
 function App() {
   const [theme, setTheme] = React.useState(localStorage.getItem('color-theme') || defaultTheme);
+  const [pageDataInfo] = React.useState(dataInfo['en']);
 
   React.useEffect(() => {
     localStorage.setItem('color-theme', theme);
@@ -15,12 +17,9 @@ function App() {
   return (
     <>
       <CurrentThemeContext.Provider value={theme}>
-        <Header
-          setTheme={setTheme}
-        />
-        <Main 
-          
-        />
+      <CurrentDataInfoContext.Provider value={pageDataInfo}>
+        <Main setTheme={setTheme} />
+      </CurrentDataInfoContext.Provider>
       </CurrentThemeContext.Provider>
     </>
   );
