94c1e8e26fc1cb883f00df589685a2c9b7dd654f
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-29 12:04:55 +0500

    refactor(EducationElementContainer): semantic changes

--- src/components/EducationElementContainer/EducationElementContainer.js
+++ src/components/EducationElementContainer/EducationElementContainer.js
@@ -1,12 +1,11 @@
 import './EducationElementContainer.css';
 import React from 'react';
 import PropTypes from 'prop-types'
-import CurrentDataInfoContext from '../../context/CurrentDataInfoContext';
 import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
 function EducationElementContainer({ data }) {
-  console.log(data);
   const theme = React.useContext(CurrentThemeContext);
+
   return (
     <li className={`education-element-container education-elem-container_theme-${theme}`}>
         <a href={data.link} className={`education-element-container__link education-element-container__link_theme-${theme}`} target={'_blank'} rel="noreferrer">
