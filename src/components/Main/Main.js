e41abb39ad5e25c70a8f1abd781b0414aadebbc8
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-07-01 10:12:20 +0500

    feat(Main): connect ContactsConteiner

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 import CurrentDataInfoContext from '../../context/CurrentDataInfoContext';
 import PortfolioContainer from '../PortfolioContainer/PortfolioContainer';
 import EducationContainer from '../EducationContainer/EducationContainer';
+import ContactsContainer from '../ContactsContainer/ContactsContainer';
 import BtnSwitchTheme from '../BtnSwitchTheme/BtnSwitchTheme';
 import { CurrentThemeContext } from '../../context/CurrentThemeContext';
 
 function Main({ setTheme }) {
   const pageDataInfo = React.useContext(CurrentDataInfoContext);
   const theme = React.useContext(CurrentThemeContext);
+
   return (
-    <main className='main'>
+    <main className={`main main_theme-${theme}`}>
       <div className='main__block-title'>
         <h1 className='main__title'>{pageDataInfo.name}</h1>
         <BtnSwitchTheme setTheme={setTheme} />
@@ -22,12 +24,13 @@ function Main({ setTheme }) {
       </h2>
       <PortfolioContainer />
       <EducationContainer />
+      <ContactsContainer />
     </main>
   );
 }
 
 Main.propTypes = {
-  setTheme: PropTypes.object,
+  setTheme: PropTypes.func,
 };
 
 export default Main;
